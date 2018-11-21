2c37a34476a1946c6f4ed8f3ba851936d0180871
aludi <ludivanleeuwen@gmail.com>
2018-11-21 11:26:53 +0100

    gamesTest.pl
    install swipl
    run program by: ?- [gamesTest].
    Examples: to find the genre of the game luna: game(luna,_,_,_,_,X).

---
+++ gamesTest.pl
@@ -0,0 +1,18 @@
+/* queries: 
+* to select a game where number of players is 5: A is 5, game(X, B, C,_,_,_), numPlay(A,B,C).
+* to find the genre of the game luna: game(luna,_,_,_,_,X).
+*
+* Format of game.6: game(name, minNumPlayers, maxNumPlayers, minPlayTime, age, category)
+*/
+
+
+numPlay(A,MIN, MAX):- A >= MIN, A =< MAX.
+
+game(spacecorp, 2, 4, 30, twaalf, science_fiction).
+game(luna, 2, 4, 60, twaalf, fantasy).
+game(betrayal_legacy, 2, 5, 45, twaalf, adventure).
+game(madeup1, 2, 30, 45, twaalf, adventure).
+game(madeup2, 2, 5, 5, vijf, adventure).
+
+
+newgame(nieuw, numPlay(A, B, C)).
