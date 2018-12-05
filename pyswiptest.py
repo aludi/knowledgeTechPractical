fd79b5638aed66e59663e1d58fbf48066d7c0387
ludi <ludivanleeuwen@gmail.com>
2018-12-05 12:53:07 +0100

    small updates

--- pyswiptest.py
+++ pyswiptest.py
@@ -1,3 +1,5 @@
+
+
 from pyswip import Prolog
 
 import tkinter as tk
@@ -129,7 +131,6 @@ def gui():
 	num = Entry(frame4)
 	num.pack()
 	tk.Button(frame4, text="Next Question", command=save_numPlayers).pack()
-	
 	frame4.pack()
 	
 	#question 5
@@ -152,19 +153,21 @@ def gui():
 	frame6.pack()
 	
 	raise_frame(frame1)
+	
 	root.mainloop()
+	
 
 
 prolog = Prolog()
 kb = load_knowledge_base()		#loading the knowledge base
-gui()							#loads the gui
+gui()						#loads the gui
 #rule for min/max
 prolog.assertz("numPlay(A,MIN, MAX):- A >= MIN, A =< MAX")
 # rule for min age
 prolog.assertz("minimumAge(M, N):- N >= M")
 
 
-	#game(name, min players, max players, time, min age, complexity, type, budget, cooperativeTF, campaignTF, Listgenre)
+#game(name, min players, max players, time, min age, complexity, type, budget, cooperativeTF, campaignTF, Listgenre)
 
 #unsubtle way of selecting for 
 
@@ -204,43 +207,3 @@ y = prolog.query(stringQuery)
 for soln in y:
 	print("you can play:", (soln["Name"]))
 	
-
-'''
-prolog.query(
-
-	"A is {},  game(Name, MinP, MaxP, Rime, Minage, Complexity, Type, Budget, CooperativeTF, CampaignTF, Listgenre), numPlay(A, MinP, MaxP),". numberOfPlayers
-
-	game(name, min players, max players, time, min age, complexity, type, budget, cooperativeTF, campaignTF, Listgenre)
-	"A is {}, Z = {}, M is {}, game(X, B, C,_,N,Z), numPlay(A, B, C), minimumAge(M, N)."
-	.format(numberOfPlayers, genre, minAge)) #inference rule
-
-x= 0
-for soln in queryOld(numberOfPlayers, genre, minAge):
-	print("you can play:", (soln["X"]))
-	x = 1
-if x == 0:
-	print("sorry, we couldn't find any games for you")
-
-'''
-
-## retracting facts?
-## only asserting facts when gui prompts or something?
-
-'''
-/* queries: 
-* to select a game where number of players is 5: A is 5, game(X, B, C,_,_,_), numPlay(A,B,C).
-* to find the genre of the game luna: game(luna,_,_,_,_,X).
-*
-* Format of game.6: game(name, minNumPlayers, maxNumPlayers, minPlayTime, age, category)
-*/
-
-
-numPlay(A,MIN, MAX):- A >= MIN, A =< MAX.
-game(spacecorp, 2, 4, 30, twaalf, science_fiction).
-game(luna, 2, 4, 60, twaalf, fantasy).
-game(betrayal_legacy, 2, 5, 45, twaalf, adventure).
-game(madeup1, 2, 30, 45, twaalf, adventure).
-game(madeup2, 2, 5, 5, vijf, adventure).
-game(madeup3, 1, 2, 10, vijf, strategy).
-
-'''
--- startingKB.pl
+++ startingKB.pl
@@ -1,6 +1,5 @@
+
 game(madeup5, 1, 2, 10, 5, 5, family, 10, true, false, strategy).
 game(madeup6, 1, 2, 10, 5, 5, family, 10, true, false, adventure).
 game(madeup7, 1, 100, 50, 5, 10, strategy, 10, false, true, strategy).
 game(madeup8, 3, 10, 20, 5, 4, strategy, 11, false, false, adventure).
-
-
