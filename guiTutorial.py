587039c59bb0fbe34d87039fe242e227629e8c9e
Ilse Pubben <Ilse.pubben@gmail.com>
2018-12-07 14:14:04 +0100

    bleh

--- guiTutorial.py
+++ guiTutorial.py
@@ -1,17 +1,29 @@
+import tkinter as tk
 from tkinter import *
 
 
 class Gui: 
 	
 	def __init__(self, master): 
-		frame = Frame(master)
-		frame.pack()
 		
-		self.printButton = Button(frame, text= "Print Message", command=self.printMessage)
-		self.printButton.pack(side=LEFT)
+		#TkInter for interface : link for tutorial :  https://www.python-course.eu/tkinter_labels.php
+		w = Label(root, font = "Times 16 bold", fg = "black", text = "Hello\n I will help you with selecting a boardgame!")
+		pic = PhotoImage(file = "img/scaryOwl1.gif") #must always be a gif, thinter doesn't like other formats.
+		w1 = Label(root, image = pic)
+		w1.image = pic
+		w1.pack(side = "right")
+		Button(root, text="ok, let's start!").pack(side = "bottom")
+		w.pack()
 		
-		self.quitButton =  Button(frame, text= "Quit", command = frame.quit)
-		self.quitButton.pack(side=LEFT)
+		
+		#frame = Frame(master)
+		#frame.pack()
+		
+		#self.printButton = Button(frame, text= "Print Message", command=self.printMessage)
+		#self.printButton.pack(side=LEFT)
+		
+		#self.quitButton =  Button(frame, text= "Quit", command = frame.quit)
+		#self.quitButton.pack(side=LEFT)
 		
 	def printMessage(self): 
 		print("wow, this actually worked")
