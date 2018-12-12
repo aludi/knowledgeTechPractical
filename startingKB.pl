b213cfb05146ea56097f5b3073a03e0997d9b56d
ludi <ludivanleeuwen@gmail.com>
2018-12-12 16:57:38 +0100

    added some real games to kb

--- startingKB.pl
+++ startingKB.pl
@@ -13,3 +13,8 @@ game(madeup18, 3, 10, 20, 16, 4.1, strategy, 11, false, false, adventure).
 game(madeup19, 2, 10, 20, 4, 4.9, wargame, 1, false, true, adventure).
 game(madeup20, 1, 2, 10, 3, 6.1, thematic, 10, true, false, adventure).
 
+game("Catan", 3, 4, 60, 10, 2.34, family, 40, false, false, trading).
+game("Monopoly", 2, 8, 60, 8, 1.67, family, 30, false, false, trading).
+game("You're Bluffing", 3, 5, 45, 10, 1.62, family, 10, false, false, animals).
+game("Risk", 2, 6, 120, 10, 2.10, wargame, 30, false, false, territory).
+
